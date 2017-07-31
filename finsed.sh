e1f5a3e449cf2dec1e2db9c57df89814618e8dc9
anilsathyan <anilsathyan7@gmail.com>
2017-07-31 17:52:19 +0530

    Update finsed.sh

--- finsed.sh
+++ finsed.sh
@@ -1,3 +1,11 @@
+#run as ./finsed.sh arg1 arg2 arg3
+#argument order start-tag, end-tag, source-file, destination-file
+
+: '
+You may use any source file with some tag's of form <tag-name> & </tag-name>
+Use the tag-name as first two arguments!!
+'
+
 echo "First arg- start-tag: $1"
 echo "Second arg- end-tag: $2"
 echo "Third arg- source: $3"
